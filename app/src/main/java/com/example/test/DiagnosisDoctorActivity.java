eb867680177bfec19e5c7b272ff57ad0d1960317
Dhriti Dhruve <dhritidhruve23@yahoo.in>
2019-02-22 17:46:41 +0530

    diagnosis

---
+++ app/src/main/java/com/example/test/DiagnosisDoctorActivity.java
@@ -0,0 +1,88 @@
+package com.example.test;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DiagnosisDoctorActivity extends AppCompatActivity {
+
+    Button addMed, addTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diagnosis_doctor);
+
+        addMed = findViewById(R.id.addMed);
+        addTest = findViewById(R.id.addTest);
+
+        addMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater li = LayoutInflater.from(getApplicationContext());
+                View promptsView = li.inflate(R.layout.prompt_med, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setView(promptsView);
+                final EditText medName =promptsView.findViewById(R.id.medName);
+                final EditText weeks =promptsView.findViewById(R.id.weeks);
+                final EditText doses = promptsView.findViewById(R.id.doses);
+                final EditText condition = promptsView.findViewById(R.id.condition);
+                builder.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+        addTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater li = LayoutInflater.from(getApplicationContext());
+                View promptsView = li.inflate(R.layout.prompts_test, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setView(promptsView);
+                final EditText medName =promptsView.findViewById(R.id.testName);
+                builder.setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+    }
+
+}
