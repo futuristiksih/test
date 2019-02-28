759d2bb33c55a520784e89a0f4dd65e601defb59
pawanshaw12 <pawan.shaw12@gmail.com>
2019-02-28 23:17:20 +0530

    adding drawable2

--- app/src/main/java/com/example/test/ChatActivity.java
+++ app/src/main/java/com/example/test/ChatActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
