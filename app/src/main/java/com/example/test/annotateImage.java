2b6193f5ff2d9f8b32c389ee37fa3caadb717676
abbas <emperorabbas@gmail.com>
2019-03-02 15:16:40 +0530

    image_dialog

--- app/src/main/java/com/example/test/annotateImage.java
+++ app/src/main/java/com/example/test/annotateImage.java
@@ -8,14 +8,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,47 +38,52 @@ public class annotateImage extends Fragment {
     //progress dialog
     private ProgressDialog progressDialog;
     //list to hold all the uploaded images
-    private List<Upload> uploads;String child_name,parent_email;ArrayList<CharSequence> filenames;
+    private List<Upload> uploads;
+    String child_name,parent_email,id;
+    ArrayList<String> filenames;
+    FirebaseFirestore db;
+    FirebaseUser user;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.annotate_image, container, false);
         getActivity().setTitle("Annotate Image");
-
+        db=FirebaseFirestore.getInstance();
         Bundle bundle=getArguments();
         child_name=bundle.getString("child_name");
         parent_email=bundle.getString("parent_email");
-        filenames=bundle.getCharSequenceArrayList("filenames");
-
+        id=bundle.getString("id");
+        filenames=new ArrayList<>();
+        user=FirebaseAuth.getInstance().getCurrentUser();
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         progressDialog = new ProgressDialog(getActivity());
         uploads = new ArrayList<>();
+
         //displaying progress dialog while fetching images
         progressDialog.setMessage("Please wait...");
         progressDialog.show();
-        mDatabase = FirebaseDatabase.getInstance().getReference("upload_images");
-        //adding an event listener to fetch values
-        mDatabase.addValueEventListener(new ValueEventListener() {
+
+
+        db.collection("Email")
+                .document("parent "+parent_email)
+                .collection("sent_appointments")
+                .document(child_name+" "+user.getEmail())
+                .collection("Dates")
+                .document(""+id).collection("Untag_images").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                //dismissing the progress dialog
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
                 progressDialog.dismiss();
-                //iterating through all the values in database
-                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                    Upload upload = postSnapshot.getValue(Upload.class);
-                    uploads.add(upload);
+                for(QueryDocumentSnapshot doc:queryDocumentSnapshots){
+                    filenames.add(doc.getString("filename"));
                 }
-                //creating adapter
-                adapter = new ImageAdapter(getActivity(), uploads);
-                //adding adapter to recyclerview
+                adapter = new ImageAdapter(getActivity(), filenames);
                 recyclerView.setAdapter(adapter);
             }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                progressDialog.dismiss();
-            }
         });
+
+
         return view;
     }
 }
